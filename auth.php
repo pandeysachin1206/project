beffb0805a7de5e4378a5096169fb311fcb563aa
pandeysachin1206 <64262454+pandeysachin1206@users.noreply.github.com>
2021-04-20 20:20:50 +0530

    Add files via upload

--- auth.php
+++ auth.php
@@ -2,21 +2,34 @@
 session_start();
 require_once("../includes/dbconn.php");
 $userlevel=$_GET['user'];
+
 // username and password sent from form 
+
 $myusername=$_POST['username']; 
-$mypassword=$_POST['password']; 
+$mypassword=$_POST['password'];
+
 
 // To protect MySQL injection (more detail about MySQL injection)
+
 $myusername = stripslashes($myusername);
 $mypassword = stripslashes($mypassword);
 
+
+if($userlevel=='1')
 $sql="SELECT * FROM users WHERE username='$myusername' AND password='$mypassword'";
+
+else
+$sql="SELECT * FROM admin WHERE username='$myusername' AND password='$mypassword'";
+
 $result=mysqli_query($conn,$sql);
 
+
 // Mysql_num_row is counting table row
+
 $count=mysqli_num_rows($result);
 $row=mysqli_fetch_assoc($result);
 $id=$row['id'];
+
 // If result matched $myusername and $mypassword, table row must be 1 row
 if($count==1){
 
